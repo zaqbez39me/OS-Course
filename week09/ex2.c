8302c0977bc35ab86d0f1276367b03b22ee4183e
Korolev Kirill <korolov_k@mail.ru>
2022-11-01 11:51:25 +0300

    completed all solutions

--- week09/ex2.c
+++ week09/ex2.c
@@ -1,18 +1,30 @@
-#include <unistd.h>
-#include <limits.h>
 #include <string.h>
+#include "unistd.h"
 #include "stdlib.h"
 
 #define SIZE (10 * 1024 * 1024)
 
 int main() {
-    int* ptr;
+    char* ptr[10];
     for(int i = 0; i < 9; ++i){
-        ptr = malloc(SIZE);
-        memset(ptr, 0, SIZE);
+        ptr[i] = malloc(SIZE);
+        memset(ptr[i], 0, SIZE);
         sleep(1);
     }
-    ptr = malloc(SIZE);
-    memset(ptr, 0, SIZE);
+    ptr[9] = malloc(SIZE);
+    memset(ptr[9], 0, SIZE);
+    for(int i = 0; i < 10; ++i)
+        free(ptr[i]);
     return EXIT_SUCCESS;
 }
+
+/* For the tests I have increased SIZE to 1.953125 GB to actually see some significant changes.
+ * Exercise 2 comments:
+ *  The number of swaps in and swaps out increased after execution. Moreover, the memory amount of memory being swapped
+ * during the execution of ex2 increased in multiple times. That means that our physical memory has no enough memory for
+ * program execution. Therefore, it has to swap the parts of memory in and out.
+ * Exercise 3 comments:
+ *  First of all, I have found that after the first cycle iteration ex2 becomes the top 1 of all executable
+ * processes. Moreover, after 9th iteration my program exceeds the size of physical memory because it occupies
+ * 17.578125 GB of virtual memory while on my computer is only 16 GB of actual RAM.
+ */
--- week09/ex4.c
+++ week09/ex4.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 #include "unistd.h"
-#include "string.h"
 #include "stdlib.h"
 #include "sys/resource.h"
 
 #define SIZE (10 * 1024 * 1024)
 
 int main() {
-    int* ptr;
+    char* ptr[10];
     struct rusage result;
     for(int i = 0; i < 9; ++i){
-        ptr = malloc(SIZE);
-        memset(ptr, 0, SIZE);
+        ptr[i] = malloc(SIZE);
+        memset(ptr[i], 0, SIZE);
         getrusage(RUSAGE_SELF, &result);
-        printf("Memory usage: %ld KB\n",result.ru_maxrss);
+        printf("Memory usage: %ld kibibytes\n", result.ru_maxrss);
         sleep(1);
     }
-    ptr = malloc(SIZE);
-    memset(ptr, 0, SIZE);
+    ptr[9] = malloc(SIZE);
+    memset(ptr[9], 0, SIZE);
     getrusage(RUSAGE_SELF, &result);
-    printf("Memory usage: %ld KB\n",result.ru_maxrss);
+    printf("Memory usage: %ld kibibytes\n", result.ru_maxrss);
+    for(int i = 0; i < 10; ++i)
+        free(ptr[i]);
     return EXIT_SUCCESS;
 }
