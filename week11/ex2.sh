08669ccdac8abd6f6e44c31c4f238ddc63edec11
Korolev Kirill <korolov_k@mail.ru>
2022-11-13 18:03:14 +0300

    completed all solutions for week 11

--- week11/ex2.sh
+++ week11/ex2.sh
@@ -5,4 +5,4 @@ printf "Kirill" > ~/week01/file.txt
 rm -f _ex2.txt
 link ~/week01/file.txt _ex2.txt > ex2.txt
 find ~/ -inum $(ls -i "_ex2.txt" | cut -d " " -f1) >> ex2.txt
-find ~/ -inum $(ls -i "$HOME/week01/file.txt" | cut -d " " -f1) -exec rm {} \; >> ex2.txt
+find . -inum $(ls -i "$HOME/week01/file.txt" | cut -d " " -f1) -exec rm {} \; >> ex2.txt
