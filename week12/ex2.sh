00ce56817b32037f810bf66cdc668d3d25de4b18
Korolev Kirill <korolov_k@mail.ru>
2022-11-23 15:47:11 +0300

    completed all solutions for week 12

--- week12/ex2.sh
+++ week12/ex2.sh
@@ -1,3 +1,4 @@
+# shellcheck disable=SC2016
 cd ~/week12/ || exit
 sh ~/week12/ex1.sh
 sudo sh -c 'echo "Kirill" > lofsdisk/file1'
@@ -7,16 +8,13 @@ sudo mkdir -p lofsdisk/lib64
 sudo mkdir -p lofsdisk/lib/x86_64-linux-gnu
 sudo gcc ex2.c -o lofsdisk/ex2.out
 cd lofsdisk || exit
-sudo cp -v /bin/bash bin/
-sudo cp -v /bin/cat bin/
-sudo cp -v /bin/echo bin/
-sudo cp -v /bin/ls bin/
-sudo cp -v /bin/touch bin/
-sudo cp -v /lib/x86_64-linux-gnu/libtinfo.so.6 lib
-sudo cp -v /lib/x86_64-linux-gnu/libc.so.6 lib
-sudo cp -v /lib64/ld-linux-x86-64.so.2 lib64
-sudo cp -v /lib/x86_64-linux-gnu/libselinux.so.1 lib
-sudo cp -v /lib/x86_64-linux-gnu/libpcre2-8.so.0 lib
+commands=("/bin/bash" "/bin/cat" "/bin/echo" "/bin/ls" "/bin/touch")
+for command in ${commands[*]}
+do
+  sudo cp -v "$command" $(printf ".%s" $(dirname "$command"))
+  sudo ldd "$command" | grep "=> \/" | xargs -P 3 sudo sh -c 'cp -v $2 $(printf ".%s" $(dirname "$2"))'
+  sudo ldd "$command" | grep "^.\/" | xargs -P 1 sudo sh -c 'cp -v $0 $(printf ".%s" $(dirname "$0"))'
+done
 cd ../
 sudo chroot lofsdisk /ex2.out > ex2.txt
 printf "\n" >> ex2.txt
